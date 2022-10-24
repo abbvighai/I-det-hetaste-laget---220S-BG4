d8819f385655cfb82e517a2f97226df6cfb0c122
abbsimmei <simon.meier@abbgymnasiet.se>
2022-10-24 16:28:45 +0200

    FUKIN Fixed IT!! LET's GOOOOOO

--- Project/Project.ino
+++ Project/Project.ino
@@ -18,7 +18,7 @@ String FIKA = "100";
 FirebaseData fbdo;
 
 
-Adafruit_SSD1306 display(128, 64, &Wire,-1,400000UL,100000UL);
+Adafruit_SSD1306 display(128, 64, &Wire, -1, 400000UL, 100000UL);
 
 AM2320 sensor;
 
@@ -34,20 +34,20 @@ FirebaseData firebaseData;
 void setup() {
   Wire.pins(14, 12);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-    
+
   Serial.begin(9600);
-  
+
   //Code to clear display
   display.clearDisplay();
-  
+
   display.setTextColor(WHITE);
 
 
 
-    // put your setup code here, to run once:
+  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
-    
+
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-fi");
 
@@ -55,7 +55,7 @@ void setup() {
     Serial.print(".");
     delay(300);
   }
-  
+
   Serial.println();
   Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
@@ -66,121 +66,115 @@ void setup() {
 
   if (!Firebase.beginStream(firebaseData, path))
   {
-      Serial.println("Could not begin stream");
-      Serial.println("REASON: " + firebaseData.errorReason());
-      Serial.println();
+    Serial.println("Could not begin stream");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println();
   }
 
 
 }
 
 
-void Temperature(void){
+void Temperature(void) {
 
 
   display.setTextSize(1);
   display.setCursor(10, 10);
   display.cp437(true);
-  if (sensor.measure()){
+  if (sensor.measure()) {
     SensorTemp = sensor.getTemperature();
     display.println(SensorTemp);
     display.println("temperature");
-    }
+  }
   display.display();
 
- }
+}
 
-void Humidity(void){
+void Humidity(void) {
 
-  
+
   display.setTextSize(1);
   display.setCursor(10, 40);
   display.cp437(true);
-  if (sensor.measure()){
+  if (sensor.measure()) {
     SensorHum = sensor.getHumidity();
     display.println(SensorHum);
     display.println("humidity");
   }
   display.display();
 
- }
-void humid(void){
-    if (sensor.measure()){
-      hum = sensor.getHumidity();
-      Serial.println(hum);
-      Serial.println("hum");
-    }
 }
-void tempurat(void){
-    if (sensor.measure()){
-      temp = sensor.getTemperature();
-      Serial.println(temp);
-      Serial.println("temp");
-    }
+void humid(void) {
+  if (sensor.measure()) {
+    hum = sensor.getHumidity();
+    Serial.println(hum);
+    Serial.println("hum");
+  }
+}
+void tempurat(void) {
+  if (sensor.measure()) {
+    temp = sensor.getTemperature();
+    Serial.println(temp);
+    Serial.println("temp");
+  }
 }
 
 void loop() {
-  
-//   delay(100);
+
+  //   delay(100);
   // humid();
- //  delay(100);
- //  tempurat();
- //  delay(100);
- //  Temperature();
- //  delay(100);
- //  Humidity();
- //  delay(100);
-    
- //  delay(2000);
- //  display.clearDisplay();
+  //  delay(100);
+  //  tempurat();
+  //  delay(100);
+  //  Temperature();
+  //  delay(100);
+  //  Humidity();
+  //  delay(100);
+
+  //  delay(2000);
+  //  display.clearDisplay();
 
 
 
-      // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly:
 
   if (!Firebase.readStream(firebaseData))
   {
-      Serial.println();
-      Serial.println("Can't read stream data");
-      Serial.println("REASON: " + firebaseData.errorReason());
-      Serial.println();
+    Serial.println();
+    Serial.println("Can't read stream data");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println();
   }
 
   if (firebaseData.streamTimeout())
   {
-      Serial.println();
-      Serial.println("Stream timeout, resume streaming...");
-      Serial.println();
+    Serial.println();
+    Serial.println("Stream timeout, resume streaming...");
+    Serial.println();
   }
 
 
   if (firebaseData.streamAvailable())
   {
-      if (firebaseData.dataType() == "json")
-      {
-        FirebaseJson &json = firebaseData.jsonObject();
-        FirebaseJsonData result;
-        
- //       FirebaseJson json;
-        
-        json.get(result, "/rum1test");
-        if (result.success)
-        {
-         if (result.intValue) {
-          Serial.println(result.intValue);
- //         Firebase.push("/Fika1/antal", FIKA);
-          if (Firebase.pushJSON(fbdo, "/Fika1/antal", json)) {
+    if (firebaseData.dataType() == "json")
+    {
+      FirebaseJson &json = firebaseData.jsonObject();
 
-          }
+      json = "{\"värde\":" + FIKA + "}";
 
 
-          
-         } 
-         else {
-           Serial.println("FUCKING HELL WTF I AM GOIN TO KILL PEOPLE");
-          }
-        }
-        
+
+      if (Firebase.setJSON(fbdo, "/Term1/Temperatur", json)) {
       }
+      if (Firebase.setJSON(fbdo, "/Term1/Luftfuktighet", json)) {
+      }
+
+
+
+      else {
+        Serial.println("FUCKING HELL WTF I AM GOIN TO KILL PEOPLE");
+      }
+    }
+
   }
-} 
+}
