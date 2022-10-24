c90e0f82183c19182f0e9833b3f3898bf33b7653
abbsimmei <simon.meier@abbgymnasiet.se>
2022-10-24 14:06:02 +0200

    från firebas till serial monitor

--- Project/Project.ino
+++ Project/Project.ino
@@ -3,6 +3,17 @@
 #include <Adafruit_SSD1306.h>
 #include <AM2320.h>
 
+#include "FirebaseESP8266.h"
+#include <ESP8266WiFi.h>
+
+#define FIREBASE_HOST "https://i-det-hetaste-laget-bg4-default-rtdb.asia-southeast1.firebasedatabase.app/"
+#define FIREBASE_AUTH "sBoymgIFPi9O8tklj9nMf4LPCWgT1SEvwyklrjga"
+#define WIFI_SSID "ABBgym_2.4"
+#define WIFI_PASSWORD "mittwifiarsabra"
+
+String path = "/rum1test";
+String nodeID = "rum1testNode";
+
 Adafruit_SSD1306 display(128, 64, &Wire,-1,400000UL,100000UL);
 
 AM2320 sensor;
@@ -12,6 +23,9 @@ float SensorHum;
 float hum;
 float temp;
 bool i = true;
+
+//Define Firebase Data objects
+FirebaseData firebaseData;
 
 void setup() {
   Wire.pins(14, 12);
@@ -23,6 +37,35 @@ void setup() {
   display.clearDisplay();
   
   display.setTextColor(WHITE);
+
+
+
+    // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+    
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-fi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+  
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+  if (!Firebase.beginStream(firebaseData, path))
+  {
+      Serial.println("Could not begin stream");
+      Serial.println("REASON: " + firebaseData.errorReason());
+      Serial.println();
+  }
 
 
 }
@@ -74,17 +117,56 @@ void tempurat(void){
 
 void loop() {
   
-    delay(100);
-    humid();
-    delay(100);
-    tempurat();
-    delay(100);
-    Temperature();
-    delay(100);
-    Humidity();
-    delay(100);
+//   delay(100);
+  // humid();
+ //  delay(100);
+ //  tempurat();
+ //  delay(100);
+ //  Temperature();
+ //  delay(100);
+ //  Humidity();
+ //  delay(100);
     
-    delay(2000);
-    display.clearDisplay();
-    
+ //  delay(2000);
+ //  display.clearDisplay();
+
+
+
+      // put your main code here, to run repeatedly:
+
+  if (!Firebase.readStream(firebaseData))
+  {
+      Serial.println();
+      Serial.println("Can't read stream data");
+      Serial.println("REASON: " + firebaseData.errorReason());
+      Serial.println();
+  }
+
+  if (firebaseData.streamTimeout())
+  {
+      Serial.println();
+      Serial.println("Stream timeout, resume streaming...");
+      Serial.println();
+  }
+
+
+  if (firebaseData.streamAvailable())
+  {
+      if (firebaseData.dataType() == "json")
+      {
+        FirebaseJson &json = firebaseData.jsonObject();
+        FirebaseJsonData result;
+        json.get(result, "/rum1test");
+        if (result.success)
+        {
+         if (result.intValue) {
+          Serial.println(result.intValue);
+         } 
+         else {
+           Serial.println("FUCKING HELL WTF I AM GOIN TO KILL PEOPLE");
+          }
+        }
+        
+      }
+  }
 } 
